8afc5964d0db347b1048c3cb9233f178552bb389
Diamond <pdm6500@outlook.com>
2018-11-03 18:44:15 +0700

    week 3 python added

---
+++ Course1Week3/FuncMinimizing/SmoothFuncMinimazing.py
@@ -0,0 +1,13 @@
+from scipy import optimize
+import numpy as np
+import math
+
+
+def f1(x):
+    return math.sin(x / 5.0) * math.exp(x / 10.0) + 5.0 * math.exp(-x / 2.0)
+
+
+optimize_result = optimize.minimize(f1, x0=np.array([30.0]))
+if optimize_result['success']:
+    x_min = optimize_result['x']
+    print(x_min)

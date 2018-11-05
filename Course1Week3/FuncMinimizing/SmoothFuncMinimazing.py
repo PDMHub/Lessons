989c9e28c213b13a5972b14341e0bbd06aca920c
Diamond <pdm6500@outlook.com>
2018-11-05 21:40:37 +0700

    some plotting with Visual Studio REPL window

--- Course1Week3/FuncMinimizing/SmoothFuncMinimazing.py
+++ Course1Week3/FuncMinimizing/SmoothFuncMinimazing.py
@@ -1,4 +1,5 @@
 from scipy import optimize
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
@@ -6,8 +7,14 @@ import math
 def f1(x):
     return math.sin(x / 5.0) * math.exp(x / 10.0) + 5.0 * math.exp(-x / 2.0)
 
+y=[]
+for x in range(0, 30, 2):
+    y.append(f1(x))
 
 optimize_result = optimize.minimize(f1, x0=np.array([30.0]))
 if optimize_result['success']:
     x_min = optimize_result['x']
     print(x_min)
+
+plt.plot(range(0, 30, 2), y)
+plt.show()

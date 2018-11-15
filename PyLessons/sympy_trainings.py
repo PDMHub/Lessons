12e020d32034fc2909b677c156360870be1237d3
Dmitry <PDMHub@users.noreply.github.com>
2018-11-15 16:55:34 +0700

    some symy trainings

--- PyLessons/sympy_trainings.py
+++ PyLessons/sympy_trainings.py
@@ -14,3 +14,21 @@ deriv_x, deriv_y
 # x,y,z = symbols('x y z ')
 # init_printing(pretty_print = True)
 # Integral(sqrt(1/x), x)
+
+#%%
+from sympy import *
+from sympy.physics.vector import ReferenceFrame, dot
+# Система отсчета
+N = ReferenceFrame('N')
+from sympy import vector
+x1, x2 , y1, y2, z1, z2 = symbols('x1 x2 y1 y2 z1 z2')
+v1 = x1*N.x + y1*N.y + z1*N.z
+v2 = x2*N.x + y2*N.y + z2*N.z
+dot(v1, v2)
+
+
+#%%
+from sympy import MatMul, MatrixSymbol
+A = MatrixSymbol('A', 3, 1)
+B = MatrixSymbol('B', 1, 3)
+MatMul(A, B)
